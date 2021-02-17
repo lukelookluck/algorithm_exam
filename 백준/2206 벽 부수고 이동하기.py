da4d2ff5fab5b3c78fe7eae6bec5bbdc9c4b99d3
Luke <gunhee9177@naver.com>
2021-02-17 23:59:39 +0900

    today's solved coding exam

--- 백준/2206 벽 부수고 이동하기.py
+++ 백준/2206 벽 부수고 이동하기.py
@@ -1,6 +1,31 @@
-from collections import deque
 import sys
 
+
+def solution():
+    my_d = []
+    my_d.append((0, 0, 1))
+    check[0][0][1] = 1
+    ans = 1
+
+    while my_d:
+        temp = []
+        for x, y, crush in my_d:
+
+            if x == N-1 and y == M-1:
+                return ans
+
+            for i in range(4):
+                new_x, new_y = x + dx[i], y + dy[i]
+                if 0 <= new_x < N and 0 <= new_y < M:
+                    if arr[new_x][new_y] == 1 and crush == 1:
+                        check[new_x][new_y][0] = check[x][y][1] + 1
+                        temp.append((new_x, new_y, 0))
+                    elif arr[new_x][new_y] == 0 and check[new_x][new_y][crush] == -1:
+                        check[new_x][new_y][crush] = check[x][y][crush] + 1
+                        temp.append((new_x, new_y, crush))
+        my_d = temp
+        ans += 1
+    return -1
 
 
 N, M = map(int, sys.stdin.readline().split())
@@ -10,30 +35,4 @@ check = [[[-1]*2 for _ in range(M)] for _ in range(N)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-
-my_d = deque()
-my_d.append((0, 0, 1))
-check[0][0][1] = 1
-
-while my_d:
-    # print(my_d)
-    # print(*check, sep='\n')
-
-    x, y, crush = my_d.popleft()
-
-    if x == N-1 and y == M-1:
-        break
-    # print('현재', x, y)
-
-    for i in range(4):
-        new_x, new_y = x + dx[i], y + dy[i]
-        if 0 <= new_x < N and 0 <= new_y < M:
-            if arr[new_x][new_y] == 1 and crush == 1:
-                check[new_x][new_y][0] = check[x][y][1] + 1
-                my_d.append((new_x, new_y, 0))
-            elif arr[new_x][new_y] == 0 and check[new_x][new_y][crush] == -1:
-                check[new_x][new_y][crush] = check[x][y][crush] + 1
-                my_d.append((new_x, new_y, crush))
-
-
-print(max(check[N-1][M-1]))
+print(solution())
