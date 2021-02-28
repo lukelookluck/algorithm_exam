900d7f41aa503f03ab0e505d439a7f6ee67c16ec
Luke <gunhee9177@naver.com>
2021-02-28 20:16:42 +0900

    today's solved coding exam

--- 백준/12865 평범한 배낭.py
+++ 백준/12865 평범한 배낭.py
@@ -1,6 +1,8 @@
 import sys
 
 
+'''
+
 def solution():
     for i in range(1, N+1):
         for j in range(1, K+1):
@@ -23,3 +25,28 @@ for n in range(N):
 
 solution()
 print(answer[N][K])
+'''
+
+
+def solution():
+    for i in range(1, N+1):
+        for j in range(K+1):
+            weight = data[i-1][0]
+            value = data[i-1][1]
+
+            if j >= weight:
+                check[i][j] = max(check[i-1][j-weight] + value, check[i-1][j])
+            else:
+                check[i][j] = check[i-1][j]
+
+
+N, K = map(int, sys.stdin.readline().split())
+data = []
+check = [[0]*(K+1) for _ in range(N+1)]
+
+for _ in range(N):
+    a, b = map(int, sys.stdin.readline().split())
+    data.append((a, b))
+
+solution()
+print(*check, sep='\n')
