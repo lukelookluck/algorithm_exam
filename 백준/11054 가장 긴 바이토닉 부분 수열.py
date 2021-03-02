56b56423dd66dabcbc265235f382b3444590ed41
Luke <gunhee9177@naver.com>
2021-03-02 20:22:39 +0900

    today's solved coding exam

---
+++ 백준/11054 가장 긴 바이토닉 부분 수열.py
@@ -0,0 +1,24 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+arr = list(map(int, sys.stdin.readline().split()))
+check = [1] * N
+check2 = [1] * N
+
+for i in range(N):
+    for j in range(i, N):
+        if arr[i] < arr[j] and check2[i] >= check2[j]:
+            check2[j] += 1
+        if arr[-i - 1] < arr[-j - 1] and check[-i - 1] >= check[-j - 1]:
+            check[-j - 1] += 1
+
+# print(check)
+# print(check2)
+
+for i in range(N):
+    check[i] += check2[i] - 1
+
+print(max(check))
+
+
+
