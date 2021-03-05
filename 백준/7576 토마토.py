771d5341666aa57e4c97be775d4a658fdb2badae
Luke <gunhee9177@naver.com>
2021-03-05 23:47:04 +0900

    today's solved coding exam

--- 백준/7576 토마토.py
+++ 백준/7576 토마토.py
@@ -2,36 +2,36 @@ from collections import deque
 import sys
 
 M, N = map(int, sys.stdin.readline().split())
-arr = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-my_deq = []
+arr = []
+my_deq = deque()
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-p_cnt, n_cnt = 0, 0
+cnt = 0
 
 for i in range(N):
+    arr.append(list(map(int, sys.stdin.readline().split())))
     for j in range(M):
         if arr[i][j] == 1:
-            p_cnt += 1
+            cnt += 1
             my_deq.append((i, j))
         elif arr[i][j] == -1:
-            n_cnt += 1
+            cnt += 1
 
 answer = -1
 
 while my_deq:
-    temp = []
-    for my in my_deq:
-        x, y = my
+    my_len = len(my_deq)
+    for _ in range(my_len):
+        x, y = my_deq.popleft()
         for i in range(4):
             new_x, new_y = x + dx[i], y + dy[i]
             if 0 <= new_x < N and 0 <= new_y < M and arr[new_x][new_y] == 0:
-                p_cnt += 1
+                cnt += 1
                 arr[new_x][new_y] = 1
-                temp.append((new_x, new_y))
+                my_deq.append((new_x, new_y))
 
-    my_deq = temp
     answer += 1
 
-if p_cnt + n_cnt == M * N:
+if cnt == M * N:
     print(answer)
 else:
     print(-1)
