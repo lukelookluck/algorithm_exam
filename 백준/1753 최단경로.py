247b0c7dce4ca3fbdfc51258a359392288533b43
Luke <gunhee9177@naver.com>
2021-03-10 00:01:31 +0900

    today's solved coding exam

--- 백준/1753 최단경로.py
+++ 백준/1753 최단경로.py
@@ -5,11 +5,11 @@ import sys
 def solution():
     while my_heap:
         my_x, my_y = heappop(my_heap)
-        for k, l in dist[my_x]:
-            my_min = my_y + l
+        for k, l in dist[my_y]:
+            my_min = my_x + l
             if check[k] > my_min:
                 check[k] = my_min
-                heappush(my_heap, [k, my_min])
+                heappush(my_heap, [my_min, k])
 
 
 V, E = map(int, sys.stdin.readline().split())
@@ -18,7 +18,7 @@ check = [float('inf')] * (V + 1)
 my_heap = []
 dist = [[] for _ in range(V+1)]
 
-heappush(my_heap, [start, 0])
+heappush(my_heap, [0, start])
 check[start] = 0
 
 for i in range(E):
