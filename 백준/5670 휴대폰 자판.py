78b5fd7286c19f7a96579151f4b01de159c6fdd9
Luke <gunhee9177@naver.com>
2021-03-22 23:59:19 +0900

    today's solved coding exam

--- 백준/5670 휴대폰 자판.py
+++ 백준/5670 휴대폰 자판.py
@@ -5,7 +5,7 @@ class Node():
         self.key = key
         self.child = {}
         self.cnt = 0
-        # self.end = False
+        self.end = False
 
 
 class Trie():
@@ -20,21 +20,17 @@ class Trie():
                 curr_node.child[s] = Node(s)
                 curr_node.cnt += 1
             curr_node = curr_node.child[s]
-        # curr_node.end = True
+        curr_node.end = True
 
     def inquiry(self, cnt, curr_node):
         global answer
-        print('answer', answer)
-        print(curr_node.key, curr_node.cnt, curr_node.end)
-        print(curr_node.child.keys())
-
         if curr_node.cnt > 1:
             cnt += 1
         if curr_node.end:
             answer += cnt
-            print('??', answer)
+            if curr_node.cnt > 0:
+                cnt += 1
         for s in curr_node.child.keys():
-            print(curr_node.child[s].cnt)
             self.inquiry(cnt, curr_node.child[s])
 
 try:
@@ -46,7 +42,6 @@ try:
             trie.insert(mystr)
         answer = 0
         trie.inquiry(0, trie.head)
-        print(answer)
         print(format(round(answer / N, 2), '.2f'))
 
 except:
