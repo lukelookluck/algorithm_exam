2c558d34b89f173b8f9bfa0b4f79e3589dc49b97
Luke <gunhee9177@naver.com>
2021-03-23 00:13:14 +0900

    today's solved coding exam

--- 백준/5670 휴대폰 자판.py
+++ 백준/5670 휴대폰 자판.py
@@ -4,8 +4,6 @@ class Node():
     def __init__(self, key):
         self.key = key
         self.child = {}
-        self.cnt = 0
-        self.end = False
 
 
 class Trie():
@@ -18,30 +16,35 @@ class Trie():
         for s in mystr:
             if s not in curr_node.child:
                 curr_node.child[s] = Node(s)
-                curr_node.cnt += 1
             curr_node = curr_node.child[s]
-        curr_node.end = True
 
     def inquiry(self, cnt, curr_node):
         global answer
-        if curr_node.cnt > 1:
-            cnt += 1
-        if curr_node.end:
-            answer += cnt
-            if curr_node.cnt > 0:
+        if curr_node == None:
+            curr_node = self.head
+            if len(curr_node.child.keys()) == 1:
                 cnt += 1
-        for s in curr_node.child.keys():
-            self.inquiry(cnt, curr_node.child[s])
+        if not len(curr_node.child.keys()):
+            answer += cnt
+        if len(curr_node.child.keys()) == 1:
+            for s in curr_node.child.values():
+                self.inquiry(cnt, s)
+        else:
+            for i, t in curr_node.child.items():
+                if i == ' ':
+                    self.inquiry(cnt, t)
+                else:
+                    self.inquiry(cnt+1, t)
 
 try:
     while True:
         N = int(sys.stdin.readline())
         trie = Trie()
         for _ in range(N):
-            mystr = sys.stdin.readline().strip()
+            mystr = sys.stdin.readline().strip() + ' '
             trie.insert(mystr)
         answer = 0
-        trie.inquiry(0, trie.head)
+        trie.inquiry(0, None)
         print(format(round(answer / N, 2), '.2f'))
 
 except:
