e8014e0492c5eecc6895ed94497fc136d4c85b65
Luke <gunhee9177@naver.com>
2021-03-23 23:00:33 +0900

    today's solved coding exam

---
+++ 백준/11401 이항 계수 3.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def solution(x, y):
+    if y == 1:
+        return x % key
+    elif y % 2:
+        return solution(x**2 % key, y // 2) * x % key
+    else:
+        return solution(x ** 2 % key, y // 2)
+
+
+N, K = map(int, sys.stdin.readline().split())
+K = min(K, N-K)
+
+key = 1000000007
+answer = 1
+temp = 1
+
+for i in range(K):
+    answer *= (N-i) % key
+    temp *= (K-i) % key
+
+print(solution(temp, key-2) * answer % key)
