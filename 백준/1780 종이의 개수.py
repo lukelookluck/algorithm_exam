fcfdc98a7edb7cf367117d7ae5d3923f197a629a
Luke <gunhee9177@naver.com>
2021-03-23 21:09:34 +0900

    today's solved coding exam

--- 백준/1780 종이의 개수.py
+++ 백준/1780 종이의 개수.py
@@ -1,1 +1,43 @@
 import sys
+
+
+def solution(s_x, e_x, s_y, e_y):
+    global m, z, p
+
+    all_same = True
+
+    if e_x - s_x > 1 and e_y - s_y > 1:
+        for i in range(s_x, e_x):
+            for j in range(s_y, e_y):
+                if arr[i][j] != arr[s_x][s_y]:
+                    all_same = False
+
+                if not all_same:
+                    temp_x, temp_y = (e_x - s_x) // 3, (e_y - s_y) // 3
+
+                    solution(s_x, s_x + temp_x, s_y, s_y + temp_y)
+                    solution(s_x, s_x + temp_x, s_y + temp_y, e_y - temp_y)
+                    solution(s_x, s_x + temp_x, e_y - temp_y, e_y)
+
+                    solution(s_x + temp_x, e_x - temp_x, s_y, s_y + temp_y)
+                    solution(s_x + temp_x, e_x - temp_x, s_y + temp_y, e_y - temp_y)
+                    solution(s_x + temp_x, e_x - temp_x, e_y - temp_y, e_y)
+
+                    solution(e_x - temp_x, e_x, s_y, s_y + temp_y)
+                    solution(e_x - temp_x, e_x, s_y + temp_y, e_y - temp_y)
+                    solution(e_x - temp_x, e_x, e_y - temp_y, e_y)
+                    return
+
+    if arr[s_x][s_y] == -1:
+        m += 1
+    elif arr[s_x][s_y] == 0:
+        z += 1
+    else:
+        p += 1
+
+
+N = int(sys.stdin.readline().strip())
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+m, z, p = 0, 0, 0
+solution(0, N, 0, N)
+print(m, z, p, sep='\n')
