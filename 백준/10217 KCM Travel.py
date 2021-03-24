b6702576a10e87e56364b00e23d79815b300fa11
Luke <gunhee9177@naver.com>
2021-03-25 01:02:43 +0900

    today's solved coding exam

--- 백준/10217 KCM Travel.py
+++ 백준/10217 KCM Travel.py
@@ -3,12 +3,7 @@ import sys
 
 
 def solution():
-    N, M, K = map(int, sys.stdin.readline().split())
-    dist = [[] for _ in range(N+1)]
-    for _ in range(K):
-        u, v, c, d = map(int, sys.stdin.readline().split())
-        dist[u].append((v, c, d))
-    check = [[sys.maxsize] * (M+1) for _ in range(N+1)]
+    check = [[float('inf')] * (M+1) for _ in range(N+1)]
     temp = [(0, 0, 1)]
 
     while temp:
@@ -20,18 +15,23 @@ def solution():
         for k, l, m in dist[idx]:
             l += c
             m += d
-            if M < l or check[k][l] <= m:
-                continue
-            for i in range(l, M+1):
-                if check[k][i] > m:
-                    check[k][i] = m
-                else:
-                    break
-            heappush(temp, (m, l, k))
-
-    return "Poor KCM"
+            if M >= l and check[k][l] > m:
+                for i in range(l, M+1):
+                    if check[k][i] > m:
+                        check[k][i] = m
+                    else:
+                        break
+                heappush(temp, (m, l, k))
+    return 'Poor KCM'
 
 
 T = int(sys.stdin.readline().strip())
 for tc in range(T):
+    N, M, K = map(int, sys.stdin.readline().split())
+    dist = {i: [] for i in range(N+1)}
+
+    for _ in range(K):
+        u, v, c, d = map(int, sys.stdin.readline().split())
+        dist[u].append((v, c, d))
+
     print(solution())
