246cca228d77b5114032e2c440457b1bdbef1940
Luke <gunhee9177@naver.com>
2021-03-25 01:01:22 +0900

    today's solved coding exam

--- 백준/10217 KCM Travel.py
+++ 백준/10217 KCM Travel.py
@@ -1,35 +1,37 @@
 from heapq import heappop, heappush
 import sys
 
+
 def solution():
-    check = [[float('inf')] * (M+1) for _ in range(N+1)]
-    check[1][1] = 0
-    temp = [[0, 0, 1]]
+    N, M, K = map(int, sys.stdin.readline().split())
+    dist = [[] for _ in range(N+1)]
+    for _ in range(K):
+        u, v, c, d = map(int, sys.stdin.readline().split())
+        dist[u].append((v, c, d))
+    check = [[sys.maxsize] * (M+1) for _ in range(N+1)]
+    temp = [(0, 0, 1)]
 
     while temp:
         d, c, idx = heappop(temp)
+        if check[idx][c] < d:
+            continue
+        if idx == N:
+            return d
         for k, l, m in dist[idx]:
             l += c
             m += d
-            if M >= l and check[k][l] > m:
-                check[k][l] = m
-                heappush(temp, [m, l, k])
+            if M < l or check[k][l] <= m:
+                continue
+            for i in range(l, M+1):
+                if check[k][i] > m:
+                    check[k][i] = m
+                else:
+                    break
+            heappush(temp, (m, l, k))
 
-    return min(check[N])
+    return "Poor KCM"
 
 
 T = int(sys.stdin.readline().strip())
 for tc in range(T):
-    N, M, K = map(int, sys.stdin.readline().split())
-    dist = {i: [] for i in range(N+1)}
-
-    for _ in range(K):
-        u, v, c, d = map(int, sys.stdin.readline().split())
-        dist[u].append([v, c, d])
-
-    result = solution()
-
-    if result <= M:
-        print(result)
-    else:
-        print('Poor KCM')
+    print(solution())
