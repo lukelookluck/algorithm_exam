237dc72f618f64cad301232d4dfc6e38dad373e0
Luke <gunhee9177@naver.com>
2021-03-25 16:48:46 +0900

    today's solved coding exam

--- 백준/1956 운동.py
+++ 백준/1956 운동.py
@@ -1,3 +1,60 @@
+from heapq import heappush, heappop
+import sys
+
+
+def solution(x):
+    check = [inf] * (V + 1)
+    temp = [(0, x)]
+
+    while temp:
+        w, idx = heappop(temp)
+        if x == idx and w != 0:
+            break
+        if check[idx] >= w:
+            for i, d in dist[idx]:
+                d += w
+                if check[i] > d:
+                    check[i] = d
+                    heappush(temp, (d, i))
+
+    return check[x]
+
+
+V, E = map(int, sys.stdin.readline().split())
+inf = float('inf')
+dist = {i: [] for i in range(1, V + 1)}
+
+for _ in range(E):
+    a, b, c = map(int, sys.stdin.readline().split())
+    dist[a].append((b, c))
+
+answer = inf
+
+for x in dist.keys():
+    if len(dist[x]):
+        answer = min(answer, solution(x))
+
+print(answer if answer != inf else -1)
+
+
+# while temp:4 3
+# 2 3 1
+# 3 4 1
+# 4 2 1
+#     c, a = heappop(temp)
+#     print(c, a)
+#     # if
+#     if check[a][0] > c:
+#         for idx, d in dist[a]:
+#             d += c
+#             if check[idx][0] == d and idx = a:
+#                 check[idx] = (d, a)
+#                 heappush(temp, (d, idx))
+#     print(check)
+#     print(temp)
+
+
+'''
 import sys
 
 
@@ -18,3 +75,4 @@ for x in range(1, V+1):
     answer = min(answer, arr[x][x])
 
 print(answer if answer != inf else -1)
+'''
