8a7e28ea64076c36a166d171b594702bd9471f44
Luke <gunhee9177@naver.com>
2021-03-29 22:46:12 +0900

    today's solved coding exam

---
+++ 백준/1644 소수의 연속합.py
@@ -0,0 +1,35 @@
+import sys
+
+
+def check(digit):
+    data = [True] * (digit + 1)
+
+    for i in range(2, int(digit ** 0.5) + 1):
+        if data[i]:
+            j = 2
+            while i * j <= digit:
+                data[i * j] = False
+                j += 1
+
+    return [i for i in range(2, digit + 1) if data[i]]
+
+
+N = int(sys.stdin.readline().strip())
+prime_number = check(N)
+prime_number.append(0)
+start, end = 0, 1
+my_sum = prime_number[0]
+answer = 0
+
+while start < end < len(prime_number):
+    if my_sum == N:
+        answer += 1
+
+    if my_sum >= N:
+        my_sum -= prime_number[start]
+        start += 1
+    else:
+        my_sum += prime_number[end]
+        end += 1
+
+print(answer)
