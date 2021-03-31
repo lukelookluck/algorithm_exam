7143b73bfbba602e3997fdd2c29c4373dc69b340
Luke <gunhee9177@naver.com>
2021-04-01 00:15:29 +0900

    today's solved coding exam

---
+++ 백준/9252 LCS 2.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def solution():
+    for i in range(len(str1)):
+        for j in range(len(str2)):
+            if str1[i] == str2[j]:
+                check[i+1][j+1] = check[i][j] + str1[i]
+            else:
+                if len(check[i][j+1]) >= len(check[i+1][j]):
+                    check[i+1][j+1] = check[i][j+1]
+                else:
+                    check[i+1][j+1] = check[i+1][j]
+    return len(check[-1][-1])
+
+
+str1 = sys.stdin.readline().strip()
+str2 = sys.stdin.readline().strip()
+check = [[''] * (len(str2) + 1) for _ in range(len(str1) + 1)]
+
+print(solution())
+print(check[-1][-1])
