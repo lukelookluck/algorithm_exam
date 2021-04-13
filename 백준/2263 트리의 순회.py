e9773b4301ce4962ae62ece5e0d3f1f838186e43
Luke <gunhee9177@naver.com>
2021-04-14 00:30:33 +0900

    today's solved coding exam

--- 백준/2263 트리의 순회.py
+++ 백준/2263 트리의 순회.py
@@ -1,52 +1,29 @@
 import sys
+sys.setrecursionlimit(10**6)
 
-n = int(sys.stdin.readline())
-in_arr = list(map(int, sys.stdin.readline().split()))
-post_arr = list(map(int, sys.stdin.readline().split()))
 
-class Node:
-    def __init__(self, value, left, right):
-        self.value = value
-        if left == value:
-            self.left = None
-        else:
-            self.left = left
-        if right == value:
-            self.right = None
-        else:
-            self.right = right
-
-def solution(in_arr, post_arr, idx):
-    if len(in_arr) <= 0 or len(post_arr) <= 0:
-        # tree[idx] = Node(idx, None, None)
+def divide(in_start, in_end, p_start, p_end):
+    print(in_start, in_end, p_start, p_end)
+    if(in_start > in_end) or (p_start > p_end):
         return
 
-    in_idx = in_arr.index(idx)
-    post_idx = post_arr.index(idx)
-    # print(in_arr, post_arr)
-    # print(in_idx, post_idx)
-    if post_arr[in_idx-1] != post_arr[post_idx-1]:
-        tree[idx] = Node(idx, post_arr[in_idx-1], post_arr[post_idx-1])
-    else:
-        tree[idx] = Node(idx, post_arr[in_idx - 1], None)
-    # print(in_arr[:in_idx], post_arr[:in_idx], post_arr[in_idx-1])
-    # print(in_arr[in_idx+1:], post_arr[in_idx:post_idx], post_arr[post_idx-1])
-    # print(tree)
-    solution(in_arr[:in_idx], post_arr[:in_idx], post_arr[in_idx-1])
-    solution(in_arr[in_idx+1:], post_arr[in_idx:post_idx], post_arr[post_idx-1])
+    parents = post_order[p_end]  # 부모노드 찾기
+    print(parents, end=" ")
+
+    left = pos[parents] - in_start  # 후위순회에서의 왼쪽인자 갯수
+    right = in_end - pos[parents]  # 후위순회에서의 오른쪽인자 갯수
+    print(left, right)
+    divide(in_start, in_start + left - 1, p_start, p_start + left - 1)  # 왼쪽 노드
+    print('asdasdasd')
+    divide(in_end - right + 1, in_end, p_end - right, p_end - 1)  # 오른쪽 노드
 
 
-def _pre(node):
-    print(node.value, end=' ')
-    if node.left != None:
-        _pre(tree[node.left])
-    if node.right != None:
-        _pre(tree[node.right])
+n = int(input())
+in_order = list(map(int, input().split()))
+post_order = list(map(int, input().split()))
+pos = [0] * (n+1)
 
-tree = {}
-# n = 11
-# in_arr = [7,4,8,2,5,1,9,6,11,10,3]
-# post_arr = [7,8,4,5,2,9,11,10,6,3,1]
-solution(in_arr, post_arr, post_arr[-1])
-# print(tree[2].right)
-_pre(tree[post_arr[-1]])
+for i in range(n):
+    pos[in_order[i]] = i
+
+divide(0, n-1, 0, n-1)
