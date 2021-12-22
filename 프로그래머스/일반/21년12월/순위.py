ffd787c3f5820c035f2f975fbc61363c9a776e68
Luke <gunhee9177@naver.com>
2021-12-22 23:08:33 +0900

    today's solved coding exam

---
+++ 프로그래머스/일반/21년12월/순위.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+def solution(n, results):
+    answer = 0
+    win_g = defaultdict(set)
+    lose_g = defaultdict(set)
+
+    for w, l in results:
+        win_g[l].add(w)
+        lose_g[w].add(l)
+
+    for i in range(1, n+1):
+        for win in win_g[i]:
+            lose_g[win].update(lose_g[i])
+        for lose in lose_g[i]:
+            win_g[lose].update(win_g[i])
+
+    for i in range(1, n+1):
+        if len(win_g[i]) + len(lose_g[i]) == n-1:
+            answer += 1
+
+    return answer
