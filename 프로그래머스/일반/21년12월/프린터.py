baa136fca2ea575bb827fab1b8b8e53b9fcef673
Luke <gunhee9177@naver.com>
2021-12-24 00:24:22 +0900

    today's solved coding exam

--- 프로그래머스/일반/21년12월/프린터.py
+++ 프로그래머스/일반/21년12월/프린터.py
@@ -4,40 +4,30 @@ from collections import deque
 def solution(priorities, location):
     answer = 0
     my_dq = deque(priorities)
-    order = location + 1
+    order = location
 
     my_max = max(my_dq)
-    print(my_dq)
+
     while len(my_dq) >= 1:
         x = my_dq.popleft()
-        print("my_max", my_max)
-        print("my_x", x)
-        print("order", order)
-        print(answer)
 
         if x == my_max:
             answer += 1
-            if order == 1:
-                print("123123")
+            if order == 0:
                 return answer
             else:
                 order -= 1
             my_max = max(my_dq)
         else:
             my_dq.append(x)
-            if order == 1:
-                order = len(my_dq)
+            if order == 0:
+                order = len(my_dq) - 1
             else:
                 order -= 1
 
-        print(my_dq)
-
-
-
-
     return answer
 
-a = [1, 1, 9, 1, 1, 1]
+a = [1, 7, 9, 1, 1, 8]
 b = 0
 
 print(solution(a, b))
