fa8ceb1df49a1009ed9a0661a3c787bd11cc701a
Luke <gunhee9177@naver.com>
2021-12-25 15:59:56 +0900

    today's solved coding exam

--- 프로그래머스/일반/21년12월/다리를 지나는 트럭.py
+++ 프로그래머스/일반/21년12월/다리를 지나는 트럭.py
@@ -4,14 +4,17 @@ from collections import deque
 def solution(bridge_length, weight, truck_weights):
     answer = 0
     my_dq = deque([0] * bridge_length)
+    my_tr = deque(truck_weights)
+    my_sum = 0
 
     while my_dq:
         answer += 1
-        my_dq.popleft()
+        my_sum -= my_dq.popleft()
 
-        if truck_weights:
-            if sum(my_dq) + truck_weights[0] <= weight:
-                my_dq.append(truck_weights.pop(0))
+        if my_tr:
+            if my_sum + my_tr[0] <= weight:
+                my_sum += my_tr[0]
+                my_dq.append(my_tr.popleft())
             else:
                 my_dq.append(0)
 
