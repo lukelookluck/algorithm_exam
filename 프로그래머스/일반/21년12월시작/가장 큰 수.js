3eadfe337fa9dc97fc95ac0a427e43697daa3933
Luke <gunhee9177@naver.com>
2022-01-06 00:16:27 +0900

    today's solved coding exam

---
+++ 프로그래머스/일반/21년12월시작/가장 큰 수.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    const answer = numbers
+        .sort((a, b) => {
+            a = a.toString();
+            b = b.toString();
+            return (b + a) - (a + b)
+        })
+        .join('');
+
+    return answer[0] === '0' ? '0' : answer;
+}
