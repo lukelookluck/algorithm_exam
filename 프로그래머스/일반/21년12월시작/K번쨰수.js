190b9f51ef4cde46a52adda8ad5a53d598dbc7da
Luke <gunhee9177@naver.com>
2022-01-05 23:52:54 +0900

    today's solved coding exam

--- 프로그래머스/일반/21년12월시작/K번쨰수.js
+++ 프로그래머스/일반/21년12월시작/K번쨰수.js
@@ -2,8 +2,7 @@ function solution(array, commands) {
     var answer = [];
 
     commands.map(c => {
-        let temp = array.slice(c[0] - 1, c[1]).sort((a, b) => a - b);
-        answer.push(temp[c[2] - 1]);
+        answer.push(array.slice(c[0] - 1, c[1]).sort((a, b) => a - b)[c[2] - 1]);
     })
 
     return answer;
