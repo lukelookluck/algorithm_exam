2b0f44dadb7e867b111a5e3db80ae8b700041f3f
Luke <gunhee9177@naver.com>
2022-01-14 23:17:16 +0900

    today's solved coding exam

---
+++ 백준/숫자 카드.py
@@ -0,0 +1,37 @@
+import sys
+
+n = int(input())
+n_arr = list(map(int, sys.stdin.readline().rstrip().split()))
+m = int(input())
+m_arr = list(map(int, sys.stdin.readline().rstrip().split()))
+
+
+def solution(n, n_arr, m, m_arr):
+    answer = []
+    n_arr = sorted(n_arr)
+    my_min, my_max = n_arr[0], n_arr[-1]
+
+    for x in m_arr:
+        my_bool = True
+        if x > my_max or x < my_min:
+            answer.append(0)
+        else:
+            left, right = 0, n - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if x == n_arr[mid]:
+                    my_bool = False
+                    right = mid - 1
+                elif x > n_arr[mid]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            if my_bool:
+                answer.append(0)
+            else:
+                answer.append(1)
+    print(*answer)
+
+
+solution(n, n_arr, m, m_arr)
+
